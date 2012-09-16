6284cd6b8ec06dd6e987eff36a76474202c6db4d
unknown <Administrator@celab2-8a.comp1-domain.spit.ac.in>
2012-09-16 15:17:41 +0530

    updated

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,11 @@
 class Company < ActiveRecord::Base
+
+  validate :symbol_length
+
+  def symbol_length
+    if symbol.length <3
+      errors.add :symbol, 'should be less than 3'
+    end
+  end
+
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,20 @@
 class Investment < ActiveRecord::Base
+
+  validate :quantity_positive
+
+  def quantity_positive
+    if quantity <0
+      errors.add :quantity,'should be positive'
+
+
+
+    end
+    validate :cost_positive
+    def cost_positive
+      if cost <0
+        errors.add :cost, 'should be positive'
+      end
+    end
+
+  end
 end
